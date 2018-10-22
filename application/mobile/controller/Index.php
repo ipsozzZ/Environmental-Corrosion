77536e4cd9d0d1db0a7d22f44db3a09716599b82
tfh93121 <tfhhh@qq.com>
2018-10-22 16:01:23 +0800

    完成公告header 与 footer

--- application/mobile/controller/Index.php
+++ application/mobile/controller/Index.php
@@ -7,11 +7,10 @@ class Index extends Controller
     public function index()
     {
         $this -> assign("pageTitle", "首页");
-        return $this -> fetch();
+        return view();
     }
     public function user () {
         $this -> assign("pageTitle", "用户");
-        return $this -> fetch('index');
-
+        return view('index');
     }
 }
