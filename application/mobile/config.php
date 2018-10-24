89be78b45e16f51774752384269db04a828663ba
tfh93121 <tfhhh@qq.com>
2018-10-24 14:22:40 +0800

    10.24 完成

--- application/mobile/config.php
+++ application/mobile/config.php
@@ -3,6 +3,7 @@
 return [
   'view_replace_str' => [
     '__IMG__' => '/static/mobile/image',
+    '__ICON__' => '/static/mobile/image/icon',
     '__CSS__' => '/static/mobile/custom/css',
     '__JS__' => '/static/mobile/custom/js',
   ],
