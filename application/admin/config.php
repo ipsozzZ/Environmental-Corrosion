004fc81773f76a249bcb95065b7b29af4dc4b461
tfh93121 <tfhhh@qq.com>
2018-10-29 17:03:01 +0800

    complate all static pages

--- application/admin/config.php
+++ application/admin/config.php
@@ -2,7 +2,7 @@
 //配置文件
 return [
     'view_replace_str' => [
-        '__ADMIN_CSS__' => '/static/admin/css',
-        '__ADMIN_JS__' => '/static/admin/js',
+        '__CSS__' => '/static/admin/css',
+        '__JS__' => '/static/admin/js',
     ],
 ];
