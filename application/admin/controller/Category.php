8d298f4b8a9af86cb27748154cbf1e2df76d9648
tfh93121 <tfhhh@qq.com>
2018-10-30 19:05:53 +0800

    update subject manager

--- application/admin/controller/Category.php
+++ application/admin/controller/Category.php
@@ -16,4 +16,38 @@ class Category extends Common
         $this -> assign('category', $Cate);
         return view();
     }
+
+    public function add ($sid = '', $name = '') {
+        if($sid != '' && $name != '') {
+            $data = [
+                'sid' => $sid,
+                'name' => $name
+            ];
+            db('lesson') -> insert($data);
+
+            return $this -> redirect("/admin/subject");
+        }
+
+        $Subjects = db('subject') -> select();
+        $this     -> assign("Subjects", $Subjects);
+
+        return view();
+    }
+
+    public function edit ($lid = 1, $sid = '', $name = '') {
+        if($sid != '' && $name != '') {
+            $data = [
+                'sid' => $sid,
+                'name' => $name
+            ];
+            db('lesson') -> where('id', $lid) -> update($data);
+            return $this -> redirect("/admin/subject");
+        }
+        $lesson = db('lesson') -> where("id", $lid) -> find();
+
+        $Subjects = db('subject') -> select();
+        $this     -> assign("Subjects", $Subjects);
+        $this -> assign('lesson', $lesson);
+        return view();
+    }
 }
