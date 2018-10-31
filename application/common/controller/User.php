d2f826a2fd014959d7c5aeee38e9b90bbd42b218
tfh93121 <tfhhh@qq.com>
2018-10-31 15:38:26 +0800

    add baby

--- application/common/controller/User.php
+++ application/common/controller/User.php
@@ -2,6 +2,7 @@
 namespace app\common\controller;
 
 use think\Controller;
+use app\common\model\User as Model;
 
 class User extends Controller
 {
@@ -12,5 +13,11 @@ class User extends Controller
   public function logout () {
     cookie('disney_token', null);
     return json_encode(1);
+  }
+
+  public function login ($user, $pass) {
+    $model = new Model();
+
+    return json_encode($model -> login($user, $pass));
   }
 }
