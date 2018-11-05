417f134eec4b92151227640d62d2a2041969a14e
tfh93121 <tfhhh@qq.com>
2018-11-05 09:21:16 +0800

    a

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -42,11 +42,22 @@ class ComposerStaticInit027879880009afed2f7d3e0b55ec35d2
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'P' => 
+        array (
+            'PHPExcel' => 
+            array (
+                0 => __DIR__ . '/..' . '/phpoffice/phpexcel/Classes',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit027879880009afed2f7d3e0b55ec35d2::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit027879880009afed2f7d3e0b55ec35d2::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit027879880009afed2f7d3e0b55ec35d2::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
