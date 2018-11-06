0dd0d8745f93c20e5d93eae831f6619b38fc79f5
tfh93121 <tfhhh@qq.com>
2018-11-06 13:57:22 +0800

    finish data

---
+++ application/common/model/Wilddata.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace app\common\model;
+
+use think\Model;
+use app\common\model\Token;
+
+/**
+ * 野外数据
+ */
+class Wilddata extends Model
+{
+
+  /**
+   * 获取所有行
+   */
+  public function getAll() {
+    $model = $this -> newInstance();
+
+    $res = $model
+      -> alias('i')
+      -> join("dcate d", "d.id = i.cid")
+      -> field("i.*, d.name as catename")
+      -> select();
+
+    return $res;
+  }
+
+  /**
+   * 根据id删除一个
+   */
+  public function deleteById ($id) {
+    $model = $this -> newInstance();
+
+    return $model -> where("id", $id) -> delete();
+  }
+
+  /**
+   * 根据id更新
+   */
+  public function updateById ($id, $data) {
+    $model = $this -> newInstance();
+
+    return $model -> save($data, ['id' => $id]);
+  }
+
+  /**
+   * 新增一个
+   */
+  public function add ($data) {
+    $model = $this -> newInstance();
+    return $model -> save($data);
+  }
+
+}
