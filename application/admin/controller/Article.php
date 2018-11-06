71e61d3b0871cc214e80512044f3937c3aa26bd3
tfh93121 <tfhhh@qq.com>
2018-11-07 00:33:53 +0800

    finish article

--- application/admin/controller/Article.php
+++ application/admin/controller/Article.php
@@ -4,6 +4,8 @@ namespace app\admin\controller;
 
 use think\Controller;
 use app\common\model\Article as Model;
+use app\common\model\Acate;
+
 
 class Article extends Common
 {
@@ -21,5 +23,65 @@ class Article extends Common
       $this -> assign('articles', $articles);
 
       return view();
+    }
+
+    public function add () {
+        $model = new Model();
+        $acateModel = new Acate();
+        $cates = $acateModel -> getAll();
+        $this -> assign('acates', $cates);
+
+        return view();
+    }
+
+    public function update() {
+        $this -> assign('modifyed', false);
+        $id = $this -> request -> post('id');
+        $cid = $this -> request -> post('cid');
+        $title = $this -> request -> post('title');
+        $authors = $this -> request -> post('authors');
+        $src = $this -> request -> post('src');
+        $cover = $this -> request -> post('cover');
+        $content = $this -> request -> post('content');
+        $data = [
+          'cid' => $cid,
+          'title' => $title,
+          'title' => $title,
+          'authors' => $authors,
+          'src' => $src,
+          'cover' => $cover,
+          'content' => $content,
+        ];
+        $model = new Model();
+        if(!$id) {
+          $res = $model -> add($data);
+        } else {
+          $res = $model -> updateById($id, $data);
+        }
+        if($res) {
+          $this -> assign('modifyed', true);
+        }
+    
+        return $this -> redirect('article/index');
+    }
+
+    public function edit ($id = 0) {
+        if($id == 0) return 0;
+        $model = new Model();
+        $article = $model -> get($id);
+        $acateModel = new Acate();
+        $cates = $acateModel -> getAll();
+        $this -> assign('acates', $cates);
+        $this -> assign('article', $article);
+
+        return view();
+    }
+
+    public function del($id = 0) {
+        if($id == 0) return 0;
+        $model = new Model();
+        $model -> where('id', $id) -> delete();
+
+        return $this -> redirect('article/index');
     }
 }
