b0813827f60b67eef29278abae6ed67658c7221c
tfh93121 <tfhhh@qq.com>
2018-11-06 13:57:22 +0800

    finish data

--- application/admin/controller/Data.php
+++ application/admin/controller/Data.php
@@ -4,6 +4,7 @@ namespace app\admin\controller;
 
 use think\Controller;
 use app\common\model\Data as Model;
+use app\common\model\Dcate;
 
 class Data extends Common
 {
@@ -26,5 +27,84 @@ class Data extends Common
       $this -> assign('data', $data);
 
       return view();
+    }
+
+    public function addIndoor () {
+        $cateModel = new Dcate();
+        $icates = $cateModel -> getByFilter(['type' => 1, 'level' => 2]); //室内数据二级分类
+
+        $this -> assign([
+            'icates' => $icates,
+        ]);
+        return view();
+    }
+    public function addWild () {
+        $cateModel = new Dcate();
+        $wcates = $cateModel -> getByFilter(['type' => 2, 'level' => 2]); //野外数据二级分类
+
+        $this -> assign([
+            'wcates' => $wcates
+        ]);
+        return view();
+    }
+
+    public function update () {
+        $id             = $this -> request -> post('id');
+        $title          = $this -> request -> post('title');
+        $cid            = $this -> request -> post('cid');
+        $type           = $this -> request -> post('type');
+        $position       = $this -> request -> post('position');
+        $cycle          = $this -> request -> post('cycle');
+        $mname          = $this -> request -> post('mname');
+        $content        = $this -> request -> post('content');
+
+
+        $data = [
+            'title'         => $title,
+            'cid'           => $cid,
+            'type'          => $type,
+            'position'      => $position,
+            'cycle'         => $cycle,
+            'content'       => $content,
+            'mname'         => $mname,
+        ];
+        $model = new Model();
+        if (!$id) {
+            $res = $model->add($data);
+            if ($res) {
+                $this->assign('result', "添加成功！");
+            } else {
+                $this->assign('result', "添加失败！");
+            }
+        } else {
+            $res = $model->updateById($id, $data);
+            if ($res) {
+                $this->assign('result', "修改成功！");
+            } else {
+                $this->assign('result', "修改失败！");
+            }
+        }
+
+        return view('result');
+    }
+
+    public function edit ($id = 0) {
+        if($id == 0) {
+            $this->assign('result', "无效的id！");
+            return view('result');
+        }
+        $cateModel = new Dcate();
+        $icates = $cateModel -> getByFilter(['type' => 1, 'level' => 2]); //室内数据二级分类
+        $wcates = $cateModel -> getByFilter(['type' => 2, 'level' => 2]); //野外数据二级分类
+
+        $this -> assign([
+            'icates' => $icates,
+            'wcates' => $wcates
+        ]);
+        $model = new Model();
+        $data = $model -> get($id);
+        $this -> assign ("data", $data);
+
+        return view();
     }
 }
