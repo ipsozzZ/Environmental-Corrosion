1a9cba3ca923b445ede35c34911c0711b66fd1c9
ipsozzZ <1975386453@qq.com>
2018-11-06 20:25:17 +0800

    update data

--- application/common/model/Data.php
+++ application/common/model/Data.php
@@ -14,37 +14,52 @@ class Data extends Model
   /**
    * 获取所有行
    */
-  public function getAll() {
-    $model = $this -> newInstance();
+  public function getAll()
+  {
+    $model = $this->newInstance();
 
-    return $model -> all();
+    return $model->all();
+  }
+
+  /**
+   * 通过cid获取符合条件的所有记录
+   * @param cid 材料分类
+   */
+  public function getAllByCid($cid)
+  {
+    $model = $this->newInstance();
+    $res = $model->where("cid", $cid)->select();
+    return $res;
   }
 
   /**
    * 根据id删除一个
    */
-  public function deleteById ($id) {
-    $model = $this -> newInstance();
+  public function deleteById($id)
+  {
+    $model = $this->newInstance();
 
-    return $model -> where("id", $id) -> delete();
+    return $model->where("id", $id)->delete();
   }
 
   /**
    * 根据id更新
    */
-  public function updateById ($id, $data) {
-    $model = $this -> newInstance();
+  public function updateById($id, $data)
+  {
+    $model = $this->newInstance();
 
-    return $model -> save($data, ['id' => $id]);
+    return $model->save($data, ['id' => $id]);
   }
 
   /**
    * 新增一个
    */
-  public function add ($data) {
-    $model = $this -> newInstance();
+  public function add($data)
+  {
+    $model = $this->newInstance();
 
-    return $model -> save($data);
+    return $model->save($data);
   }
 
 }
--- application/index/controller/Outdoor.php
+++ application/index/controller/Outdoor.php
@@ -3,26 +3,39 @@ namespace app\index\controller;
 
 use think\Controller;
 use app\index\controller\Common;
+use app\common\controller\Dcate as commonDcate;
 
 class Outdoor extends Common
 {
-  public function _initialize(){
+  public function _initialize()
+  {
     parent::_initialize();
   }
 
   /**
    * 室外数据
    */
-  public function index(){
-    $this -> assign('currTitle','室外数据');
+  public function index()
+  {
+    $modelDcate = new commonDcate();
+    $parentDcate = json_decode($modelDcate->getByLevelAndType(1, 1));
+    $childDcate = json_decode($modelDcate->getByLevelAndType(1, 2));
+    $dataShow = json_decode($modelDcate->getWildAll());
+    $this->assign([
+      'currTitle' => '室外数据',
+      'parentDcate' => $parentDcate,
+      'childDcate' => $childDcate,
+      'dataShow' => $dataShow,
+    ]);
     return view();
   }
 
-   /**
+  /**
    * 显示数据
    */
-  public function dataShow(){
-    $this -> assign('currTitle','St12琼海96');
+  public function dataShow()
+  {
+    $this->assign('currTitle', 'St12琼海96');
     return view('dataShow');
   }
 }
