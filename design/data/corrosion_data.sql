1f00e2d8c085a548533fdaab8d925a50c7de27af
tfh93121 <tfhhh@qq.com>
2018-11-06 20:22:41 +0800

    sync

--- design/data/corrosion_data.sql
+++ design/data/corrosion_data.sql
@@ -1,1 +1,48 @@
-INSERT INTO corrosion.data (id, title, position, cycle, cid, ctime, viewer, content, type, mname) VALUES (1, 'asdsads', '111', 4, 1, '2018-11-05 17:38:45', 3, 'sadsasa', 1, 'Na');
+INSERT INTO corrosion.data (id, title, position, cycle, cid, ctime, viewer, content, type, mname) VALUES (1, 'asdsads', '111', 4, 11, '2018-11-06 13:33:20', 3, '<table border="0" width="100%" cellpadding="0" cellspacing="0">
+                    <tbody>
+                      <tr>
+                        <th>&nbsp;项目</th>
+                        <th>数据&nbsp;</th>
+                      </tr>
+                      <tr>
+                        <td>&nbsp; &nbsp; 123</td>
+                        <td>&nbsp;456</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                    </tbody>
+                  </table><p><br></p>', 1, 'Na');
+INSERT INTO corrosion.data (id, title, position, cycle, cid, ctime, viewer, content, type, mname) VALUES (2, 'asd', 'sss', 3, 2, '2018-11-06 13:13:03', 0, '<table border="0" width="100%" cellpadding="0" cellspacing="0">
+                    <tbody>
+                      <tr>
+                        <th>&nbsp;项目</th>
+                        <th>数据&nbsp;</th>
+                      </tr>
+                      <tr>
+                        <td>&nbsp; &nbsp; 123</td>
+                        <td>&nbsp;456</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                      <tr>
+                        <td>&nbsp;</td>
+                        <td>&nbsp;</td>
+                      </tr>
+                    </tbody>
+                  </table><p><br></p>', 1, 'ss#');
